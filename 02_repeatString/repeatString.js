91a823f5e985f1fd3c1c7ddc2a450d2e835b35fc
Rupert <rupert.hoffmann.06@gmail.com>
2024-08-10 19:21:14 +0200

    Finished 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let word = string;
+    for(let i = num; i > 1; i--){
+        word += string;
+    }
+    return word;
 };
 
 // Do not edit below this line
