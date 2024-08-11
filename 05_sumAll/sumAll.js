08463b693f6f85c2776dfee1623aff43f45a51bb
Rupert <rupert.hoffmann.06@gmail.com>
2024-08-11 15:19:48 +0200

    Finished 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(numa, numb) {
+    if(numa < 0 || numb < 0 || !Number.isInteger(numa) || !Number.isInteger(numb)){
+        return "ERROR";
+    }
+    if (numa > numb) {
+        [numa, numb] = [numb, numa];
+    }
+    if (numa == numb){
+        return numa;
+    }
+    else {
+        return numa + sumAll(numa + 1, numb);
+    }
 };
 
 // Do not edit below this line
