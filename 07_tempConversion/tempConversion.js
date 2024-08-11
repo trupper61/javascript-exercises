ce28f926f3836d24ac9f8db5a4877bf450dd1d62
Rupert <rupert.hoffmann.06@gmail.com>
2024-08-11 15:44:22 +0200

    Finishe 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  const celsius = (fahr -32) * (5/9);
+  return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const fahr = celsius * (9/5) + 32;
+  return parseFloat(fahr.toFixed(1));
 };
 
 // Do not edit below this line
