0c82bea1d767dedf19b86becdb7af7e9c0fab92d
Rupert <rupert.hoffmann.06@gmail.com>
2024-08-13 23:06:42 +0200

    Finished 09_palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
 
+    const reversedStr = cleanedStr.split('').reverse().join('');
+
+    return cleanedStr === reversedStr;
 };
 
 // Do not edit below this line
