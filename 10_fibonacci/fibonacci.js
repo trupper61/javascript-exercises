4ab86a7855a2ba0bbd921e6f2ed2e6d9d83e16d4
Rupert <rupert.hoffmann.06@gmail.com>
2024-08-13 23:12:50 +0200

    Finished 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    if (n < 0) return "OOPS";
 
+    let a = 0, b = 1;
+
+    for (let i = 2; i <= n; i++) {
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return b;
 };
 
 // Do not edit below this line
