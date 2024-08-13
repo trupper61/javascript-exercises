6cff8f2cbca67b42a39b1d86b8b2f93ebd5b76ea
Rupert <rupert.hoffmann.06@gmail.com>
2024-08-13 23:00:46 +0200

    Finished 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(numa, numb) {
+	return numa+numb
 };
 
-const subtract = function() {
-	
+const subtract = function(numa, numb) {
+	return numa-numb
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, curent) => total + curent, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, curent) => product * curent);
 };
 
-const power = function() {
-	
+const power = function(numa, numb) {
+	return Math.pow(numa, numb);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if(n === 0) {
+    return 1;
+  }
+  let result = 1;
+  for (let i = n; i > 0; i--){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
