a9d9463039eb569eafe405b6be775154e2358380
Rupert <rupert.hoffmann.06@gmail.com>
2024-08-13 23:25:45 +0200

    Finished 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
 
+    return people.reduce((oldest, person) => {
+        const personAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+
+        return personAge > oldestAge ? person : oldest;
+    });
 };
 
 // Do not edit below this line
